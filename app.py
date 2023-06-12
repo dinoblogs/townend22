a73a538265bec6362aabd3d7bfd48ef085124776
dinoblogs <113527802+dinoblogs@users.noreply.github.com>
2023-06-12 13:29:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,16 @@ from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from fileinput import filename
 import requests
-
+import cloudinary
+import cloudinary.uploader
 from datetime import datetime
 
 
-
+cloudinary.config( 
+  cloud_name = "dofvicxek", 
+  api_key = "465383777877424", 
+  api_secret = "a6ErazY7iy9WCT-cneQMZA7sZZQ" 
+)
 
 app = Flask(__name__)
 app.secret_key = "krishna4704"
@@ -64,14 +69,18 @@ def handle_post():
     name1 = name1.replace(" ", "")
     name1 = name1.replace(":", "")
     name1 = name1.replace(".", "")
-    f.save(f'static/{name1}.png')  
+    f.save(f'static/{name1}.png')
+    cloudinary.uploader.upload(f"static/{name1}.png", 
+                               public_id = name1)  
     #file
+    image_url = f'https://res.cloudinary.com/dofvicxek/image/upload/c_thumb,h_250,w_216/{name1}.png'
+
     title = request.form['name']
     sp = request.form['sp']
     rank = request.form['rank']
     mrp = request.form['mrp']
     desc = request.form['desc']
-    product = Product(title=title, desc=desc, img_url=f'static/{name1}.png',rank = rank, SP=sp,MRP=mrp)
+    product = Product(title=title, desc=desc, img_url=f'{image_url}',rank = rank, SP=sp,MRP=mrp)
     db.session.add(product)
     db.session.commit()
     return redirect('/add')
